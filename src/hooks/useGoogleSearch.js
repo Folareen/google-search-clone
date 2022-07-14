a67fe57834e69e1516bb554904297a891ecaa037
Folareen <sakawahab03@gmail.com>
2022-07-14 23:31:27 +0100

    useGoogleSearch hook

---
+++ src/hooks/useGoogleSearch.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+const baseUrl = "https://google-search3.p.rapidapi.com/api/v1/";
+const useGoogleSearch = (term, type) => {
+  const [loading, setLoading] = useState(false);
+  const [result, setResult] = useState("");
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    googleSearch();
+  }, []);
+
+  const googleSearch = async () => {
+    try {
+      setLoading(true);
+      const data = await fetch(`${baseUrl}${type}/q=${term}`, {
+        method: "GET",
+        headers: {
+          "X-User-Agent": "desktop",
+          "X-Proxy-Location": "EU",
+          "X-RapidAPI-Key":
+            "a3f5923269mshb2b9a1649ef26b6p14ec30jsn84d8543adf73",
+          "X-RapidAPI-Host": "google-search3.p.rapidapi.com",
+        },
+      });
+      const res = await data.json();
+      setResult(res);
+      setLoading(false);
+    } catch {
+      setError(true);
+      setLoading(false);
+    }
+  };
+
+  return { loading, result, error };
+};
+
+export default useGoogleSearch;
--- src/pages/All.jsx
+++ src/pages/All.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import SearchNav from '../components/SearchNav'
 import StyledAll from '../styles/StyledAll.styled'
 import SearchField from '../components/SearchField'
+import useGoogleSearch from '../hooks/useGoogleSearch'
 
-const AllResults = () => {
+const AllResults = ({children}) => {
+  const {loading, result, error} = useGoogleSearch("elon", "search")
+
+  useEffect(
+    () => {
+      console.log(
+        loading, result, error
+      )
+    }
+  )
+
   return (
     <StyledAll>
       <SearchField />
