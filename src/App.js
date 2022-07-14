b42bc3296c73380bee4685b39ec3aefe23fe49be
Folareen <sakawahab03@gmail.com>
2022-07-14 20:09:42 +0100

    news result page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import All from "./pages/All";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Images from "./pages/Images";
 import Videos from "./pages/Videos";
+import News from "./pages/News";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route element={<All />} exact path="/all" />
         <Route element={<Images />} path="/images" />
         <Route element={<Videos />} path="/videos" />
+        <Route element={<News />} path="/news" />
       </Routes>
     </Router>
   );
---
+++ src/styles/StyledNews.styled.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const StyledVideos = styled.div`
+  padding: 20px 0;
+  min-height: 100vh;
+  background-color: rgb(32, 33, 37);
+
+  div {
+    border-top: 1px solid rgb(139, 155, 170);
+    border-bottom: 1px solid rgb(139, 155, 170);
+    padding: 10px;
+    margin-top: -2px;
+  }
+`;
+
+export default StyledVideos;
