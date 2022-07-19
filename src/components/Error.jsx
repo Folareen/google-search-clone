071dc2b9331d261f3596cbebfd006882eb124f2c
Folareen <sakawahab03@gmail.com>
2022-07-19 01:26:33 +0100

    error component

---
+++ src/components/Error.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import StyledError from '../styles/StyledError.styled'
+
+const Error = () => {
+  return (
+    <StyledError>
+        <p>
+            Error...
+        </p>
+    </StyledError>
+  )
+}
+
+export default Error
---
+++ src/styles/StyledError.styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+const StyledError = styled.div`
+  background-color: rgb(32, 33, 37);
+  color: brown;
+  height: 100vh;
+  diplay: flex;
+  align-items: center;
+  justify-center: center;
+`;
+
+export default StyledError;
