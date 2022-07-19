1dac13b96bb204f56204f39b8dd36c17c16e1f3b
Folareen <sakawahab03@gmail.com>
2022-07-19 01:39:22 +0100

    error

--- src/styles/StyledError.styled.js
+++ src/styles/StyledError.styled.js
@@ -3,9 +3,14 @@ const StyledError = styled.div`
   background-color: rgb(32, 33, 37);
   color: brown;
   height: 100vh;
-  diplay: flex;
+  display: flex;
   align-items: center;
-  justify-center: center;
+  justify-content: center;
+
+  p {
+    font-size: 1.2rem;
+    letter-spacing: 3px;
+  }
 `;
 
 export default StyledError;
